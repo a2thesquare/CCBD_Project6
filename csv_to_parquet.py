07f6c24232600386ff48d674d71b81dc31a7aca1
Angeliki Andreadi <angeliki.andreadi@unine.ch>
2026-04-23 20:10:44 +0200

    csv to parquet works, datasets now in parquet (but locally, must solve)

--- csv_to_parquet.py
+++ csv_to_parquet.py
@@ -2,27 +2,27 @@ import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
 
+#csv_file = '/Users/angelikiandreadi/Downloads/100M.csv'
+#parquet_file = '/Users/angelikiandreadi/Downloads/100M.parquet'
 
+#csv_file = '/Users/angelikiandreadi/Downloads/25M.csv'
+#parquet_file = '/Users/angelikiandreadi/Downloads/25M.parquet'
 
-csv_file = '/Users/angelikiandreadi/Downloads/100M.csv'
-parquet_file = '/Users/angelikiandreadi/Downloads/100M.parquet'
-chunk_size = 1_000_000  # Process 1 million rows at a time
+csv_file = '/Users/angelikiandreadi/Downloads/5M.csv'
+parquet_file = '/Users/angelikiandreadi/Downloads/5M.parquet'
 
-# Create a CSV reader object
+chunk_size = 1_000_000  
+
 reader = pd.read_csv(csv_file, chunksize=chunk_size)
 
 writer = None
 
 for chunk in reader:
-    # Convert Pandas chunk to PyArrow Table
     table = pa.Table.from_pandas(chunk)
-    
-    # On the first chunk, initialize the Parquet writer
     if writer is None:
         writer = pq.ParquetWriter(parquet_file, table.schema)
     
     writer.write_table(table)
 
-# Close the writer to finalize the file
 if writer:
     writer.close()
